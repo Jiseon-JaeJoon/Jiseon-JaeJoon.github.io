03bfb5b20564dcefa2ce1e15897de80a9162d268
손재준 <sonjajon3231@gmail.com>
2025-12-28 23:37:58 +0900

    251228_초안완성

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+import mainImage from './assets/지선재준.jpg'
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="wedding-container">
+      {/*여기에 내용을 채워나갈 거에요 */}
+      <h1>Jiseon & JaeJoon Wedding Invitation</h1>
+
+      <img 
+        src={mainImage} 
+        alt="Main Image" 
+        style={{width: '100%', maxWidth: '400px', borderRadius: '10px'}}
+      />
+      <p> 뿌빠뿌빠뿌빠뿌뿌뿌</p>
+      </div>
+  )
+}
+
+export default App
